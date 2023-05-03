d5372b9149b2188a4523e1ebf8bf73b472d8cffa
MBI-88 <ingmbi8807@gmail.com>
2023-05-03 17:31:45 -0300

    fixing Online App title

--- app/pages/projects/components/onlineapp.tsx
+++ app/pages/projects/components/onlineapp.tsx
@@ -13,7 +13,7 @@ const OnlineApp = () => {
                     animate={{ color: ['#40e612', '#7a19db9e', '#d31a1ab0', '#2551e2fe'], animationTimingFunction: 'ease' }}
                     transition={{ duration: 10, repeat: Infinity, delay: 1 }}
                 >
-                    Applications Online
+                    Online Apps
                 </motion.h2>
             </section>
             <motion.ul className='flex flex-col flex-wrap items-center' variants={animation[0]} initial="hidden" animate="show">
@@ -21,7 +21,7 @@ const OnlineApp = () => {
                     app.map((item, index) => (
                         <motion.li variants={animation[index + 1]} className='mt-5' key={"li_"+index.toString()}>
                             <a href={item.url} >
-                                <h3 className=' text-blue-500 hover:text-red-500'>{item.name}</h3> 
+                                <h3 className='text-blue-500 hover:text-red-500'>{item.name}</h3> 
                             </a>
                         </motion.li>
                     ))
