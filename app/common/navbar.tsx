bcbe9310993c831ed1ff259c3373fc417a9ea887
MBI-88 <ingmbi8807@gmail.com>
2023-05-03 22:34:05 -0300

    narvar done

--- app/common/navbar.tsx
+++ app/common/navbar.tsx
@@ -5,14 +5,20 @@ import Link from "next/link";
 import style from '../styles/Home.module.css'
 import { motion } from 'framer-motion'
 import { HiOutlineDownload } from 'react-icons/hi'
+import { useState } from "react";
+
 
 
 const Navbar = () => {
+    const [open,setOpen] = useState<boolean>(false)
+    const handleCollapse = () => {
+        setOpen(!open)
+    }
 
     return (
         <nav className={style.navbackground + "  fixed w-full z-20 top-0 left-0 border-b border-gray-200"}>
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
-                <section className="flex flex-row items-center">
+                <section className="flex flex-row items-center sm:hover:cursor-pointer md:pointer-events-none" onClick={handleCollapse}>
                     <motion.div className=" bg-black rounded-full p-1"
                         initial={{ borderColor: 'ActiveBorder', borderWidth: 2, borderStyle: 'solid' }}
                         animate={{
@@ -20,13 +26,13 @@ const Navbar = () => {
                             animationTimingFunction: 'ease-out'
                         }}
                         transition={{ duration: 3.5, repeat: Infinity }}
+                        
                     >
                         <Image src="/static/img/Porfolio_MBI2.webp"
                             className="mr-1 rounded-2xl"
                             width={"35"} height={"35"} alt="MBI image"
                         />
                     </motion.div>
-
                     <motion.h2
                         className="ps-1"
                         animate={{ color: ['#40e612', '#7a19db9e', '#d31a1ab0', '#2551e2fe'], animationTimingFunction: 'ease' }}
@@ -36,22 +42,7 @@ const Navbar = () => {
                     </motion.h2>
                 </section>
 
-                <div className="flex md:order-2">
-                    <motion.button data-collapse-toggle="navbar-sticky"
-                        type="button"
-                        className="inline-flex items-center p-2 text-sm  rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200"
-                        aria-controls="navbar-sticky"
-                        aria-expanded="false"
-                        animate={{ color: ['#40e612', '#7a19db9e', '#d31a1ab0', '#2551e2fe'], animationTimingFunction: 'ease' }}
-                        transition={{ duration: 10, repeat: Infinity, delay: 1 }}
-                        
-                    >
-                        <span className="sr-only">Menu</span>
-                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-                    </motion.button>
-
-                </div>
-                <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                <div className={open ? "items-center justify-between w-full md:flex md:w-auto md:order-1 mx-auto" : "items-center justify-between hidden w-full md:flex md:w-auto md:order-1 mx-auto"}>
                     <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0">
                         <li>
                             <Link href="/" className="block py-2 pl-3 pr-4  rounded bg-transparent  md:p-0 ">
