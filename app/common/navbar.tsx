995f7b0405b5d9a507f92ecbff169e7e709503e3
MBI-88 <ingmbi8807@gmail.com>
2023-05-03 21:33:37 -0300

    fixing navbar

--- app/common/navbar.tsx
+++ app/common/navbar.tsx
@@ -44,6 +44,7 @@ const Navbar = () => {
                         aria-expanded="false"
                         animate={{ color: ['#40e612', '#7a19db9e', '#d31a1ab0', '#2551e2fe'], animationTimingFunction: 'ease' }}
                         transition={{ duration: 10, repeat: Infinity, delay: 1 }}
+                        
                     >
                         <span className="sr-only">Menu</span>
                         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
