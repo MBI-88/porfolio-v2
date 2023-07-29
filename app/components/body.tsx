b7a86685a02dc6d40bb81a75536a208fb1f5c7b7
MBI-88 <ingmbi8807@gmail.com>
2023-07-29 12:22:46 -0300

    changed layout text

--- app/components/body.tsx
+++ app/components/body.tsx
@@ -12,29 +12,15 @@ const Body = () => {
                 Background
             </motion.h2>
             <article className="pt-5 pb-5 lg:ps-20 ps-0 md:ps-16">
-                <h3 className="font-medium text-justify">
-                    I am a Telecommunications and Electronics engineer.
-                    I am a fan of technologies and the internet. When I learned how to programme,
-                    I never stopped doing it. Of all the aspects that my career offers me, I chose programming.
-                    I started with small applications using C++, since it was the first language I learned. Then,
-                    I made small console applications to manage user registries and small databases.
-                    But I decided that C++ was not the language I identified with,
-                    it took too long to be developed. As far as C# concerns,
-                    I was more comfortable using it and I was able to make larger scale applications with graphical interfaces,
-                    one of them was a tool for a Telecommunications Fundamentals course where you could measure bandwidth
-                    and bit rate among other variables.
-                    Then I found Python and with it offered a lot of advantages for different branches of programming.
-                    Python was everything I was looking for. After a time of experience, I reproduced several applications
-                    that I had already done before and others in the field of Cybersecurity, where it has great domain. I worked
-                    with artificial intelligence and data science, in web development with frameworks like Django, Flask and FastApi,
-                    also in the development of desktop applications with the PyQT5 and PySide framework. Certainly, Python is an all-rounder.
-                    Also, while working with this technology,
-                    I was programming relational databases such as MySQL, PostgresSQL and non-relational databases such as MongoDb.
-                    I had to learn JavaScript, HTML5 and CSS5 as part of web development, since Django did not provide me with all the necessary
-                    tools for a good graphic design.
-                    As a result, I got acquainted with React and Angular.
-                    I have worked with React more because it is my main front-end tool.
-                    Currently, I have continuous improvement of my skills and I often look for information about new technologies.
+                <h3 className="font-medium">
+                    I'm a Telecommunications and Electronics engineer.
+                    I'm a fan of technologies and the internet.
+                    I've worked
+                    with artificial intelligence and data science, in web development with frameworks like Django, Flask, FastApi and Fiber.
+                    I've been working with relational databases such as MySQL, PostgresSQL and non-relational databases such as MongoDB.
+                    I've been working with front-end frameworks like Angular, Vue and Nextjs.
+                    <br/><br/>
+                    <q><b>My favorite tech stack is Go (Fiber) and Nexjs</b></q>
                 </h3>
                 
             </article>
